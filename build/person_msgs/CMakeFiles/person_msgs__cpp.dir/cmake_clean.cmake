b3abe50f45939aeee2c0d603fe12f97e5cd36951
Takumi Ochiai <s21C1028YG@s.chibakoudai.jp>
2022-12-07 09:49:31 +0900

    Initial Commit

---
+++ build/person_msgs/CMakeFiles/person_msgs__cpp.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/person_msgs__cpp"
+  "rosidl_generator_cpp/person_msgs/msg/person.hpp"
+  "rosidl_generator_cpp/person_msgs/msg/person__struct.hpp"
+  "rosidl_generator_cpp/person_msgs/msg/person__traits.hpp"
+  "rosidl_generator_cpp/person_msgs/srv/query.hpp"
+  "rosidl_generator_cpp/person_msgs/srv/query__struct.hpp"
+  "rosidl_generator_cpp/person_msgs/srv/query__traits.hpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/person_msgs__cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/person_msgs/CMakeFiles/person_msgs__rosidl_typesupport_fastrtps_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "rosidl_typesupport_fastrtps_cpp/person_msgs/msg/dds_fastrtps/person__type_support.cpp"
+  "rosidl_typesupport_fastrtps_cpp/person_msgs/msg/person__rosidl_typesupport_fastrtps_cpp.hpp"
+  "rosidl_typesupport_fastrtps_cpp/person_msgs/srv/dds_fastrtps/query__type_support.cpp"
+  "rosidl_typesupport_fastrtps_cpp/person_msgs/srv/query__rosidl_typesupport_fastrtps_cpp.hpp"
+  "CMakeFiles/person_msgs__rosidl_typesupport_fastrtps_cpp.dir/rosidl_typesupport_fastrtps_cpp/person_msgs/msg/dds_fastrtps/person__type_support.cpp.o"
+  "CMakeFiles/person_msgs__rosidl_typesupport_fastrtps_cpp.dir/rosidl_typesupport_fastrtps_cpp/person_msgs/srv/dds_fastrtps/query__type_support.cpp.o"
+  "libperson_msgs__rosidl_typesupport_fastrtps_cpp.pdb"
+  "libperson_msgs__rosidl_typesupport_fastrtps_cpp.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/person_msgs__rosidl_typesupport_fastrtps_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
